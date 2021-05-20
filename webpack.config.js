dd5173c636d93bd5646fd7f2acfbe6e10db7615b
Artur Oliveira Gomes <>
2021-05-20 19:03:37 -0400

    pushing to deploy @ vercel

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,48 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
-const isDevelopment = process.env.NODE_ENV !== 'develpment';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const isDevelopment = process.env.NODE_ENV === 'develpment';
 
 module.exports = {
-  mode: 'development',
-  devtool: 'eval-source-map',
-  entry: path.resolve(__dirname, 'src', 'index.tsx'),
-  output: { path: path.join(__dirname, 'dist'), filename: 'bundle.js' },
-  resolve: {
-    extensions: ['.js', '.jsx','.tsx','.ts']
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
-    hot:true,
-  },
-  plugins: [
-    isDevelopment && new ReactRefreshWebpackPlugin(),
-    new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, 'public', 'index.html')
-  })].filter(Boolean),
-  module: {
-    rules: [
-      {
-        test: /\.(j|t)sx$/,
-        exclude: /node_modules/,
-        use: {
-          loader:'babel-loader',
-          options: {
-            plugins: [
-              isDevelopment && require.resolve('react-refresh/babel')
-            ].filter(Boolean)
-          }
-        }
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-    ]
-  }
-
-}
+    mode: 'development',
+    devtool: 'eval-source-map',
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: { path: path.join(__dirname, 'dist'), filename: 'bundle.js' },
+    resolve: {
+        extensions: ['.js', '.jsx', '.tsx', '.ts'],
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        hot: true,
+    },
+    plugins: [
+        isDevelopment && new ReactRefreshWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        }),
+    ].filter(Boolean),
+    module: {
+        rules: [
+            {
+                test: /\.(j|t)sx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment &&
+                                require.resolve('react-refresh/babel'),
+                        ].filter(Boolean),
+                    },
+                },
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+};
